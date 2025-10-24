88267c58ab5e05bfb97d67ae142d621503a51825
evepoluszczak <eve.rosenbach@gmail.com>
2025-10-24 13:50:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -288,13 +288,13 @@ else:
         render_simulateur_objectif_page()
 
    elif page == "Analyse Budgétaire":
-    import importlib, traceback, streamlit as st
-    try:
-        ab = importlib.import_module("ui.pages.analyse_budgetaire")
-        render_analyse_budgetaire_page = getattr(ab, "render_analyse_budgetaire_page")
-    except Exception:
-        st.error("❌ Erreur lors du chargement de la page Analyse Budgétaire")
-        st.code(traceback.format_exc())
-        st.stop()
-    render_analyse_budgetaire_page()
+        import importlib, traceback, streamlit as st
+        try:
+            ab = importlib.import_module("ui.pages.analyse_budgetaire")
+            render_analyse_budgetaire_page = getattr(ab, "render_analyse_budgetaire_page")
+        except Exception:
+            st.error("❌ Erreur lors du chargement de la page Analyse Budgétaire")
+            st.code(traceback.format_exc())
+            st.stop()
+        render_analyse_budgetaire_page()
 
